9e452e9ff37b28933908db528d9d5b57bee44982
we25317735 <113696278+we25317735@users.noreply.github.com>
2025-01-06 23:10:33 +0800

    移除東西

--- db-helpers/sequelize/associations-setup.js
+++
@@ -1,5 +0,0 @@
-export default function applyAssociations(sequelize) {
-  // const { instrument, orchestra } = sequelize.models
-  // orchestra.hasMany(instrument)
-  // instrument.belongsTo(orchestra)
-}
